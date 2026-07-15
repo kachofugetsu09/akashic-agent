4c956006a31a998a0fc72a433cfa66ae944ae587
huashen <2494946808@qq.com>
2026-07-15 13:06:51 +0800

    feat(android): send commands from composer panel

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.akashic.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.6.1"
+        versionCode = 8
+        versionName = "0.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
