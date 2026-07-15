1a5161e15b8c55938187dea93d6a8b29ecebc97a
huashen <2494946808@qq.com>
2026-07-16 01:01:38 +0800

    chore(android): release 0.7.6

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.akashic.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "0.7.5"
+        versionCode = 15
+        versionName = "0.7.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
