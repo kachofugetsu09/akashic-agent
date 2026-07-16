d6701d007949c35a3cdddb4702f3aeee829bd01f
huashen <2494946808@qq.com>
2026-07-17 02:05:47 +0800

    chore(android): prepare 0.7.9 release

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "com.akashic.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.7.8"
+        versionCode = 18
+        versionName = "0.7.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
